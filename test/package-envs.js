6ba7663c2579c239f2b206ba42d87af4cc9d0e4e
isaacs <i@izs.me>
2020-08-10 16:46:43 -0700

    Include a subset of `package.json` fields in env
    This implements the change described in https://github.com/npm/rfcs/pull/183

---
+++ test/package-envs.js
@@ -0,0 +1,46 @@
+const t = require('tap')
+const packageEnvs = require('../lib/package-envs.js')
+
+t.strictSame(packageEnvs({}, {
+  name: 'name',
+  version: 'version',
+  config: {
+    number: 420.69,
+    null: null,
+    empty: '',
+    zero: 0,
+    false: false,
+    nested: {object: {is: {included: true }}},
+  },
+  bin: {
+    foo: './bar.js',
+    bar: false,
+  },
+  engines: ['array', 'of', 'strings'],
+  some: { other: 'thing' },
+}), {
+  npm_package_name: 'name',
+  npm_package_version: 'version',
+  npm_package_config_number: '420.69',
+  npm_package_config_null: '',
+  npm_package_config_empty: '',
+  npm_package_config_zero: '0',
+  npm_package_config_false: '',
+  npm_package_config_nested_object_is_included: 'true',
+  npm_package_engines: 'array\n\nof\n\nstrings',
+  npm_package_bin_foo: './bar.js',
+  npm_package_bin_bar: '',
+})
+
+const foo = { foo: 'bar' }
+const ret = packageEnvs(foo, {
+  name: 'name',
+  version: 'version',
+})
+t.notEqual(ret, foo, 'returns new object')
+t.strictSame(ret, {
+  foo: 'bar',
+  npm_package_name: 'name',
+  npm_package_version: 'version',
+}, 'new env object has new stuff')
+t.strictSame(foo, { foo: 'bar' }, 'original unmodified')
