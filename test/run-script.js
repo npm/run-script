037193e54339682361ac587952b5f6390867dc9e
Ruy Adorno <ruyadorno@hotmail.com>
2020-10-29 11:37:15 -0400

    fix: do not throw on missing package.json
    This fixes running `npm exec` in folders that does not have a
    `package.json` since `runScript` throws a `ENOENT` error.

--- test/run-script.js
+++ test/run-script.js
@@ -1,10 +1,11 @@
 const requireInject = require('require-inject')
 
-const runScript = requireInject('../lib/run-script.js', {
+const mocks = {
   '../lib/run-script-pkg.js': async x => x,
   '../lib/validate-options.js': () => {},
   'read-package-json-fast': async x => x,
-})
+}
+const runScript = requireInject('../lib/run-script.js', mocks)
 
 const t = require('tap')
 
@@ -13,6 +14,17 @@ t.test('no package provided, look up the package', t =>
     path: 'foo',
     pkg: 'foo/package.json',
   })))
+
+t.test('no package provided, look up the package fails', t => {
+  const runScript = requireInject('../lib/run-script.js', {
+    ...mocks,
+    'read-package-json-fast': () => Promise.reject(new Error('ERR'))
+  })
+  return runScript({ path: 'foo' }).then(res => t.strictSame(res, {
+    path: 'foo',
+    pkg: { scripts: {} },
+  }))
+})
 
 t.test('package provided, skip look up', t =>
   runScript({ path: 'foo', pkg: 'bar' }).then(res => t.strictSame(res, {
